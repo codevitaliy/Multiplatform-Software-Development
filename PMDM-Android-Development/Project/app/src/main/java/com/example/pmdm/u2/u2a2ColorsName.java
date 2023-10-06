15a95ddce5d43c9bca4bbfc57713c2b7ae99d5f2
codevitaliy <codevitaliy@gmail.com>
2023-10-06 12:28:05 +0200

    added graphic name color

---
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u2/u2a2ColorsName.java
@@ -0,0 +1,16 @@
+package com.example.pmdm.u2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm.R;
+
+public class u2a2ColorsName extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_u2a2_colors_name);
+  }
+}
