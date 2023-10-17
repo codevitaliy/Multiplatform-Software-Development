e3664d03ce007945b966af2ea46738f22d4ea4ec
codevitaliy <codevitaliy@gmail.com>
2023-10-17 13:16:55 +0200

    Adding constraints to u2a3

---
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u2/u2a3Propinatron2000.java
@@ -0,0 +1,16 @@
+package com.example.pmdm.u2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm.R;
+
+public class u2a3Propinatron2000 extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_u2a3_propinatron2000);
+  }
+}
