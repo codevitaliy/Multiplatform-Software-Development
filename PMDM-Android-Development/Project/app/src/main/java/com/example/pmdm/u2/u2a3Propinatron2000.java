d298ff8df08c75e9a2a6af998b379162e88504ec
codevitaliy <codevitaliy@gmail.com>
2023-10-19 11:09:53 +0200

    finished propinatron

--- PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u2/u2a3Propinatron2000.java
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u2/u2a3Propinatron2000.java
@@ -1,29 +1,28 @@
 package com.example.pmdm.u2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
-import android.view.WindowManager;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.view.View.OnClickListener;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pmdm.R;
 
 public class u2a3Propinatron2000 extends AppCompatActivity {
 
-  private static final int  MAX_NUMBERS = 8;
+  private static final int MAX_NUMBERS = 8;
   EditText etNumberInput;
   TextView tvDisplay;
-  Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt0,btC,btD,buttonCalculate;
-  RadioButton btPoor,btNice,btGreat;
-
+  Button bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, bt0, btC, btD, buttonCalculate;
+  RadioButton radioButton;
+  RadioGroup radioGroup;
+  RadioButton btPoor, btNice, btGreat;
+  double tipPercentage;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +46,8 @@ public class u2a3Propinatron2000 extends AppCompatActivity {
     //Max number in the Edit text is in the xml layout android:maxLength="10"
     etNumberInput = findViewById(R.id.u2a3etNumberInput);
     etNumberInput.setShowSoftInputOnFocus(false);
+
+    radioGroup = findViewById(R.id.radioGroup);
 
     btPoor = findViewById(R.id.u2a3rbPoor);
     btNice = findViewById(R.id.u2a3rbNice);
@@ -85,20 +86,60 @@ public class u2a3Propinatron2000 extends AppCompatActivity {
       int stringLength = originalString.length();
       String substringString;
 
-      if(stringLength > 0) {
+      if (stringLength > 0) {
         //take the last character from a string
         substringString = originalString.substring(0, stringLength - 1);
         etNumberInput.setText(substringString);
       }
     });
 
+    //sets the percentage of the tip depending on the radio button selected
+    setTipPercentage();
 
-
-
-
-
-
+    //calculate the tip and show it in the display
+    calculateTip();
 
 
   }
+
+  public void setTipPercentage() {
+    radioGroup.setOnCheckedChangeListener((radioGroup, checkedId) -> {
+
+      if (checkedId == R.id.u2a3rbPoor) {
+        this.tipPercentage = 0.15;
+        tvDisplay.setText("");
+      } else if (checkedId == R.id.u2a3rbNice) {
+        this.tipPercentage = 0.20;
+        tvDisplay.setText("");
+      } else if (checkedId == R.id.u2a3rbGreat) {
+        this.tipPercentage = 0.25;
+        tvDisplay.setText("");
+      }
+    });
+  }
+
+  public double getTipPercentage() {
+    return tipPercentage;
+  }
+
+  public double getTotalAmout() {
+    String totalAmount = etNumberInput.getText().toString();
+    return Double.parseDouble(totalAmount);
+  }
+
+  public void calculateTip() {
+
+    buttonCalculate.setOnClickListener(v -> {
+
+      double totalTip = getTotalAmout() * getTipPercentage();
+      String totalStr = String.valueOf(totalTip);
+
+      tvDisplay.setText(totalStr);
+
+    });
+
+
+  }
+
+
 }
