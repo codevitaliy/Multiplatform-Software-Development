64e01ae2ccae7e9612654490bc022957606c4c2f
codevitaliy <codevitaliy@gmail.com>
2023-10-24 14:38:11 +0200

    wip adding alert dialog u2a5

--- PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u2/u2a5RockPaperScissors.java
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u2/u2a5RockPaperScissors.java
@@ -1,7 +1,10 @@
 package com.example.pmdm.u2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,9 +17,16 @@ import com.example.pmdm.R;
 public class u2a5RockPaperScissors extends AppCompatActivity {
 
   static final int MAX_SCORE = 3;
+  static int playerScore = 0;
+  static int cpuScore = 0;
+  static final int ROCK = 1;
+  static final int PAPER = 2;
+  static final int SCISSORS = 3;
+  static int choose;
   ImageButton btnRockCpu,btnPaperCpu,btnScissorsCpu,btnRockPlayer,btnPaperPlayer,btnScissorsPlayer;
   Button reset;
   TextView tvCpuScore, tvPlayerScore, tvDisplay;
+  int playerChoice;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,7 @@ public class u2a5RockPaperScissors extends AppCompatActivity {
     btnPaperPlayer = findViewById(R.id.u2a5iBtnPaperPlayer);
     btnScissorsPlayer = findViewById(R.id.u2a5iBtnScissorsPlayer);
 
+
     //Reset button
     reset = findViewById(R.id.u2a5btnReset);
 
@@ -40,22 +51,112 @@ public class u2a5RockPaperScissors extends AppCompatActivity {
     tvPlayerScore = findViewById(R.id.u2a5tvPlayerScore);
     tvDisplay = findViewById(R.id.u2a5tvDisplay);
 
+    //disable cpu buttons
+    btnRockCpu.setEnabled(false);
+    btnPaperCpu.setEnabled(false);
+    btnScissorsCpu.setEnabled(false);
+
+    reset.setOnClickListener(v -> {
+      if (playerScore != 0 || cpuScore != 0) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Set the title and message for the dialog
+        builder.setTitle("RESET")
+                .setMessage("SURE TO RESET?");
+        // SET POSITIVE BUTTON AND ITS CLICK LISTENER
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            setReset();
+          }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+          }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+      } else {
+        setReset();
+      }
+    });
 
 
 
+    playerTurn();
 
   }
 
-  public int playerTurn () {
+  public int cpuTurn() {
+    int cpuChoice;
+    cpuChoice = (int) (Math.random()*3) + 1;
+    return cpuChoice;
+  }
 
-    int playerChoose;
-
-    btnRockPlayer.setOnClickListener(v -> {
-
-      playerChoose = 1;
-
+  public int playerTurn() {
+    btnRockPlayer.setOnClickListener(v-> {
+      playerChoice = ROCK;
+      playRound();
     });
 
+    btnPaperPlayer.setOnClickListener(v ->{
+      playerChoice = PAPER;
+      playRound();
+    });
+
+    btnScissorsPlayer.setOnClickListener(v->{
+      playerChoice = SCISSORS;
+      playRound();
+    });
+
+    return playerChoice;
+  }
+  public void playRound() {
+
+      int playerChoice = playerTurn();
+      int cpuChoice = cpuTurn();
+
+      if (cpuChoice == playerChoice) {
+        tvDisplay.setText(R.string.u2a5Draw);
+      } else if (playerChoice == ROCK && cpuChoice == SCISSORS ||
+              playerChoice == PAPER && cpuChoice == ROCK ||
+              playerChoice == SCISSORS && cpuChoice == PAPER) {
+        tvDisplay.setText(R.string.u2a5PlayerWinsRound);
+        playerScore++;
+        tvPlayerScore.setText(String.valueOf(playerScore));
+      } else {
+        tvDisplay.setText(R.string.u2a5CpuWinsRound);
+        cpuScore++;
+        tvCpuScore.setText(String.valueOf(cpuScore));
+      }
+      if (playerScore == MAX_SCORE) {
+
+        tvDisplay.setText(R.string.u2a5PlayerWins);
+
+        btnRockPlayer.setEnabled(false);
+        btnPaperPlayer.setEnabled(false);
+        btnScissorsPlayer.setEnabled(false);
+      } else if (cpuScore == MAX_SCORE) {
+        tvDisplay.setText(R.string.u2a5CpuWins);
+
+        btnRockPlayer.setEnabled(false);
+        btnPaperPlayer.setEnabled(false);
+        btnScissorsPlayer.setEnabled(false);
+      }
+  }
+
+  public void setReset() {
+
+    cpuScore = 0;
+    playerScore = 0;
+
+    btnRockPlayer.setEnabled(true);
+    btnPaperPlayer.setEnabled(true);
+    btnScissorsPlayer.setEnabled(true);
 
   }
 
