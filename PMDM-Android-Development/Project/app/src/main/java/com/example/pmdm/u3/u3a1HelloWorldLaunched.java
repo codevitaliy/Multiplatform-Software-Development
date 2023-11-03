32c1dd9309cb97ce4d604b488e9de5d6cc3991b0
codevitaliy <codevitaliy@gmail.com>
2023-11-03 09:02:54 +0100

    added hello world launcher activity

---
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u3/u3a1HelloWorldLaunched.java
@@ -0,0 +1,29 @@
+package com.example.pmdm.u3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.pmdm.R;
+
+public class u3a1HelloWorldLaunched extends AppCompatActivity {
+
+  Button btnCloseActivity;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.u3a1_hello_world_launched);
+
+    btnCloseActivity = findViewById(R.id.u3a1LaunchedBtnClose);
+
+    btnCloseActivity.setOnClickListener(view -> {
+
+      finish();
+
+    });
+
+
+  }
+}
