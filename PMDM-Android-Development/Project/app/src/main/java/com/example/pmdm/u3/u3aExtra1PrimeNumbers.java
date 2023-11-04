f4cc3b0072dff4a58285c72c940ff506e3601f76
codevitaliy <codevitaliy@gmail.com>
2023-11-04 16:28:57 +0100

    solved error u3a1ExtraPrimeNumbers

--- PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u3/u3aExtra1PrimeNumbers.java
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u3/u3aExtra1PrimeNumbers.java
@@ -1,12 +1,9 @@
 package com.example.pmdm.u3;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
