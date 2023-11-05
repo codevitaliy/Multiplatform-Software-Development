6cd0549c3736f042abe4ac4991e6a541fcc25b7c
codevitaliy <codevitaliy@gmail.com>
2023-11-05 19:06:30 +0100

    wip u3a6TextAnalyzer graphic layout finished

---
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u3/u3a6TextAnalyzer.java
@@ -0,0 +1,16 @@
+package com.example.pmdm.u3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm.R;
+
+public class u3a6TextAnalyzer extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.u3a6_text_analyzer);
+  }
+}
