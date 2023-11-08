c42419f173f0c380d640430599cab7221866aded
codevitaliy <codevitaliy@gmail.com>
2023-11-08 10:17:34 +0100

    wip u3a7 added layout for weapons

---
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u3/u3a7left4deadSelectWeapon.java
@@ -0,0 +1,22 @@
+package com.example.pmdm.u3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.pmdm.R;
+
+public class u3a7left4deadSelectWeapon extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    //SET FULL SCREEN
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    setContentView(R.layout.u3a7left4dead_select_weapon);
+  }
+}
