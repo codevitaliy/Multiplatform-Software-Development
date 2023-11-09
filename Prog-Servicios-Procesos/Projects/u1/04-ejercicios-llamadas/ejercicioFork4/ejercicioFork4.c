4eaec0890a985f5bf9e0b0e3573be33f17b5567f
codevitaliy <codevitaliy@gmail.com>
2023-11-09 18:34:26 +0100

    wip c fork() exercises

--- Prog-Servicios-Procesos/Projects/u1/04-ejercicios-llamadas/ejercicioFork4/ejercicioFork4.c
+++ Prog-Servicios-Procesos/Projects/u1/04-ejercicios-llamadas/ejercicioFork4/ejercicioFork4.c
@@ -4,54 +4,19 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main() {
-    int num_hijos = 3;
+#define NUM_SONS 5
 
-    for (int i = 1; i <= num_hijos; i++) {
-        pid_t pid = fork();
-
-        if (pid == -1) {
-            perror("Error al crear al hijo");
-            return 1;
-        }
-
-        if (pid == 0) {
-            // Esto se ejecuta en el proceso hijo
-            switch (i) {
-                case 1:
-                    // Código para el primer hijo
-                    execlp("ls", "ls", "/", NULL);
-                    perror("Error en exec");
-                    exit(1);
-                case 2:
-                    // Código para el segundo hijo
-                    {
-                        FILE *archivo = fopen("salida.txt", "w");
-                        if (archivo == NULL) {
-                            perror("Error abriendo archivo");
-                            exit(1);
-                        }
-                        fprintf(archivo, "Hola mundo\n");
-                        fclose(archivo);
-                    }
-                    exit(0); // Importante: el hijo debe salir después de realizar su tarea
-                case 3:
-                    // Código para el tercer hijo
-                    printf("Soy el hijo %d (PID: %d)\n", i, getpid());
-                    exit(0);
-                default:
-                    // Este caso no debería ocurrir
-                    fprintf(stderr, "Número de hijo no válido: %d\n", i);
-                    exit(1);
-            }
-        } else {
-            // Esto se ejecuta en el proceso padre
-            wait(NULL); // Esperar a que el hijo termine
+int main()
+{
+    for(int i=0;i<NUM_SONS;i++) // loop will run n times (n=5)
+    {
+        if(fork() == 0)
+        {
+            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+            exit(0);
         }
     }
-
-    // Esto se ejecuta solo en el proceso padre
-    printf("Soy el proceso padre (PID: %d)\n", getpid());
-
-    return 0;
+    for(int i=0;i<5;i++) // loop will run n times (n=5)
+    wait(NULL);
+     
 }
