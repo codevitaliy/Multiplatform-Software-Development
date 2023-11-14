424beda04c1d96a3aa9387185b4e6fef38e0186b
codevitaliy <codevitaliy@gmail.com>
2023-11-14 11:46:43 +0100

    wip u3a7 left4dead app

--- PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u3/u3a7left4deadMain.java
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u3/u3a7left4deadMain.java
@@ -6,21 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.pmdm.R;
 
 public class u3a7left4deadMain extends AppCompatActivity {
-  public static final Integer PLAYER_ONE_CHOICE = null;
-  public static final Integer PLAYER_TWO_CHOICE =null;
-  ImageButton btnPlayerOne;
-  ImageButton btnPlayerTwo;
-  ImageButton btnWeaponOne;
-  ImageButton btnWeaponTwo;
+
+  public enum SelectedType {
+    CHARACTER,
+    WEAPON;
+  }
+
+  static final String PLAYER_ONE = "PLAYER_ONE";
+  static final String PLAYER_TWO = "PLAYER_TWO";
+  static final String WEAPON_ONE = "WEAPON_ONE";
+  static final String WEAPON_TWO = "WEAPON_TWO";
+  static final String INFO_PLAYER = "com.example.pmdm.u3.INFO_PLAYER";
+  String selectedPlayer = "";
+  int selectedWeaponImage;
+  int selectedCharacterImage;
+  String receivedDataCharacters;
+  String receiveDataWeapons;
+  ImageButton btnPlayerOne, btnPlayerTwo, btnWeaponOne, btnWeaponTwo;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -33,46 +45,148 @@ public class u3a7left4deadMain extends AppCompatActivity {
     btnWeaponOne = findViewById(R.id.u3a7btnWeapon1);
     btnWeaponTwo = findViewById(R.id.u3a7btnWeapon2);
 
-    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+    btnPlayerOne.setTag(SelectedType.CHARACTER);
+    btnPlayerTwo.setTag(SelectedType.CHARACTER);
+    btnWeaponOne.setTag(SelectedType.WEAPON);
+    btnWeaponTwo.setTag(SelectedType.WEAPON);
+
+
+
+    ActivityResultLauncher<Intent> launcherSelectCharacters = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
               if(result.getResultCode() == RESULT_OK) {
                 Intent data = result.getData();
                 if(data != null) {
-                  Integer receivedData = data.getIntExtra(u3a7left4deadSelectCharacter.CHARACTER_PLAYER_ONE,DEFAULT_KEYS_DISABLE);
-                  System.out.println(receivedData);
-                  if(receivedData != null) {
-                    if(receivedData == 2) {
-                      int newImageResource = R.drawable.character_3;
-                      btnPlayerOne.setImageResource(newImageResource);
-                    }
-                    //Toast.makeText(u3a7left4deadMain.this, R.string.CANCEL_MESSAGE, Toast.LENGTH_SHORT).show();
+                  receivedDataCharacters = data.getStringExtra(u3a7left4deadSelectCharacter.INFO_CHARACTER_SELECTED);
+                  String receivedCharacter = data.getStringExtra(u3a7left4deadSelectCharacter.INFO_PLAYER_SELECTED_CHARACTER);
+                  if(receivedDataCharacters != null || receivedCharacter != null) {
+                      int newImageResource;
+
+                      if(receivedCharacter.equals(PLAYER_ONE)) {
+                        switch (receivedDataCharacters) {
+                          case "CHARACTER1":
+                            newImageResource = R.drawable.character_1;
+                            btnPlayerOne.setImageResource(newImageResource);
+                            break;
+                          case "CHARACTER2":
+                            newImageResource = R.drawable.character_2;
+                            btnPlayerOne.setImageResource(newImageResource);
+                            break;
+                          case "CHARACTER3":
+                            newImageResource = R.drawable.character_3;
+                            btnPlayerOne.setImageResource(newImageResource);
+                            break;
+                          case "CHARACTER4":
+                            newImageResource = R.drawable.character_4;
+                            btnPlayerOne.setImageResource(newImageResource);
+                        }
+                      }else if (receivedCharacter.equals(PLAYER_TWO)) {
+                        switch (receivedDataCharacters) {
+                          case "CHARACTER1":
+                            newImageResource = R.drawable.character_1;
+                            btnPlayerTwo.setImageResource(newImageResource);
+                            break;
+                          case "CHARACTER2":
+                            newImageResource = R.drawable.character_2;
+                            btnPlayerTwo.setImageResource(newImageResource);
+                            break;
+                          case "CHARACTER3":
+                            newImageResource = R.drawable.character_3;
+                            btnPlayerTwo.setImageResource(newImageResource);
+                            break;
+                          case "CHARACTER4":
+                            newImageResource = R.drawable.character_4;
+                            btnPlayerTwo.setImageResource(newImageResource);
+                        }
+                      }
                   }
                 }
               }
             }
     );
 
-    btnPlayerOne.setOnClickListener(view -> {
-      Intent intent = new Intent(u3a7left4deadMain.this, u3a7left4deadSelectCharacter.class);
-      launcher.launch(intent);
-    });
+    ActivityResultLauncher<Intent> launcherSelectWeapons = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+              if (result.getResultCode() == RESULT_OK) {
+                Intent data = result.getData();
+                if (data != null) {
+                  String receivedWeapon = data.getStringExtra(u3a7left4deadSelectWeapon.INFO_WEAPON_SELECTED);
+                  // Change the line below to get the correct information for the selected character
+                  String receiveDataWeapons = data.getStringExtra(u3a7left4deadSelectWeapon.INFO_PLAYER_SELECTED_WEAPON);
+                  //
+                  if (receiveDataWeapons != null || receivedWeapon != null) {
+                   int newImageResource;
 
-    btnPlayerTwo.setOnClickListener(view ->{
-      Intent intent = new Intent(u3a7left4deadMain.this, u3a7left4deadSelectCharacter.class);
-      launcher.launch(intent);
-    });
+                    if (receiveDataWeapons.equals(PLAYER_ONE)) {
+                      switch (receivedWeapon) {
+                        case "WEAPON1":
+                          newImageResource = R.drawable.weapon_1;
+                          btnWeaponOne.setImageResource(newImageResource);
+                          break;
+                        case "WEAPON2":
+                          newImageResource = R.drawable.weapon_2;
+                          btnWeaponOne.setImageResource(newImageResource);
+                          break;
+                        case "WEAPON3":
+                          newImageResource = R.drawable.weapon_3;
+                          btnWeaponOne.setImageResource(newImageResource);
+                          break;
+                        case "WEAPON4":
+                          newImageResource = R.drawable.weapon_4;
+                          btnWeaponOne.setImageResource(newImageResource);
+                          break;
+                      }
+                    } else if (receiveDataWeapons.equals(PLAYER_TWO)) {
+                      switch (receivedWeapon) {
+                        case "WEAPON1":
+                          newImageResource = R.drawable.weapon_1;
+                          btnWeaponTwo.setImageResource(newImageResource);
+                          break;
+                        case "WEAPON2":
+                          newImageResource = R.drawable.weapon_2;
+                          btnWeaponTwo.setImageResource(newImageResource);
+                          break;
+                        case "WEAPON3":
+                          newImageResource = R.drawable.weapon_3;
+                          btnWeaponTwo.setImageResource(newImageResource);
+                          break;
+                        case "WEAPON4":
+                          newImageResource = R.drawable.weapon_4;
+                          btnWeaponTwo.setImageResource(newImageResource);
+                          break;
+                      }
+                    }
+                  }
+                }
+              }
+            }
+    );
 
-    btnWeaponOne.setOnClickListener(view -> {
-      Intent intent = new Intent(u3a7left4deadMain.this, u3a7left4deadSelectWeapon.class);
-      launcher.launch(intent);
-    });
 
+    View.OnClickListener handlerPlayer = view -> {
+      if (view.getId() == R.id.u3a7btnPlayer1 || view.getId() == R.id.u3a7btnWeapon1) {
+        selectedPlayer = PLAYER_ONE;
+      } else if (view.getId() == R.id.u3a7btnPlayer2 || view.getId() == R.id.u3a7btnWeapon2) {
+        selectedPlayer = PLAYER_TWO;
+      }
 
-    btnWeaponTwo.setOnClickListener(view -> {
-      Intent intent = new Intent(u3a7left4deadMain.this, u3a7left4deadSelectWeapon.class);
-      launcher.launch(intent);
-    });
+      if (view.getTag() == SelectedType.CHARACTER) {
+        Intent intent = new Intent(u3a7left4deadMain.this, u3a7left4deadSelectCharacter.class);
+        intent.putExtra(INFO_PLAYER, selectedPlayer);
+        launcherSelectCharacters.launch(intent);
+      } else if (view.getTag() == SelectedType.WEAPON) {
+        Intent intent = new Intent(u3a7left4deadMain.this, u3a7left4deadSelectWeapon.class);
+        intent.putExtra(INFO_PLAYER, selectedPlayer);
+        launcherSelectWeapons.launch(intent); // You were missing this line
+      }
+    };
+
+    btnPlayerOne.setOnClickListener(handlerPlayer);
+    btnPlayerTwo.setOnClickListener(handlerPlayer);
+    btnWeaponOne.setOnClickListener(handlerPlayer);
+    btnWeaponTwo.setOnClickListener(handlerPlayer);
 
 
   }
--- PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u3/u3a7left4deadSelectCharacter.java
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u3/u3a7left4deadSelectCharacter.java
@@ -3,8 +3,8 @@ package com.example.pmdm.u3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
@@ -13,13 +13,14 @@ import com.example.pmdm.R;
 
 public class u3a7left4deadSelectCharacter extends AppCompatActivity {
 
-  static final String CHARACTER_PLAYER_ONE = "com.example.pmdm.u3.CHARACTER_PLAYER_ONE";
-  ImageButton btnCharacter1;
-  ImageButton btnCharacter2;
-  ImageButton btnCharacter3;
-  ImageButton btnCharacter4;
-  ImageButton btnGoBack;
-  ImageButton btnClear;
+  static final String INFO_CHARACTER_SELECTED = "com.example.pmdm.u3.INFO_CHARACTER_SELECTED";
+  static final String INFO_PLAYER_SELECTED_CHARACTER = "com.example.pmdm.u3.INFO_PLAYER_SELECTED";
+
+  public enum PlayerCharacter {
+    CHARACTER1, CHARACTER2, CHARACTER3, CHARACTER4;
+  }
+  String playerSelection;
+  ImageButton imgBtnCharacter1, imgBtnCharacter2, imgBtnCharacter3, imgBtnCharacter4, imgBtnGoBack, imgBtnClean;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,44 +28,69 @@ public class u3a7left4deadSelectCharacter extends AppCompatActivity {
     // Make the activity full-screen
     fullScreen();
 
-    btnCharacter1 = findViewById(R.id.u3a7btnCharacter1);
-    btnCharacter2 = findViewById(R.id.u3a7btnCharacter2);
-    btnCharacter3 = findViewById(R.id.u3a7btnCharacter3);
-    btnCharacter4 = findViewById(R.id.u3a7btnCharacter4);
-    btnGoBack = findViewById(R.id.u3a7btnGoBackCharacter);
-    btnClear = findViewById(R.id.u3a7btnClearCharacter);
+    imgBtnCharacter1 = findViewById(R.id.u3a7btnCharacter1);
+    imgBtnCharacter2 = findViewById(R.id.u3a7btnCharacter2);
+    imgBtnCharacter3 = findViewById(R.id.u3a7btnCharacter3);
+    imgBtnCharacter4 = findViewById(R.id.u3a7btnCharacter4);
+    imgBtnGoBack = findViewById(R.id.u3a7btnGoBackCharacter);
+    imgBtnClean = findViewById(R.id.u3a7btnClearCharacter);
+
+    imgBtnCharacter1.setTag(PlayerCharacter.CHARACTER1);
+    imgBtnCharacter2.setTag(PlayerCharacter.CHARACTER2);
+    imgBtnCharacter3.setTag(PlayerCharacter.CHARACTER3);
+    imgBtnCharacter4.setTag(PlayerCharacter.CHARACTER4);
+
+    Intent intent = getIntent();
+
+    if(intent != null) {
+      playerSelection = intent.getStringExtra(u3a7left4deadMain.INFO_PLAYER);
+    }
+
+
+    View.OnClickListener handler = (View view) -> {
+      PlayerCharacter selectedCharacter = (PlayerCharacter) view.getTag();
+      //
+
+      switch (selectedCharacter) {
+        case CHARACTER1:
+          handleCharacterSelection(PlayerCharacter.CHARACTER1);
+          backgroundColor(imgBtnCharacter1);
+          break;
+        case CHARACTER2:
+          handleCharacterSelection(PlayerCharacter.CHARACTER2);
+          backgroundColor(imgBtnCharacter2);
+          break;
+        case CHARACTER3:
+          handleCharacterSelection(PlayerCharacter.CHARACTER3);
+          backgroundColor(imgBtnCharacter3);
+          break;
+        case CHARACTER4:
+          handleCharacterSelection(PlayerCharacter.CHARACTER4);
+          backgroundColor(imgBtnCharacter4);
+          break;
+        default:
+          // for default
+          break;
+      }
+    };
+
+    imgBtnCharacter1.setOnClickListener(handler);
+    imgBtnCharacter2.setOnClickListener(handler);
+    imgBtnCharacter3.setOnClickListener(handler);
+    imgBtnCharacter4.setOnClickListener(handler);
+
+
+
+  }
+
+  private void handleCharacterSelection(PlayerCharacter selectedCharacter) {
+    // You can perform any actions related to character selection here
+    // For example, you can pass the selected character to another activity
     Intent data = new Intent();
-
-    btnCharacter1.setOnClickListener(view -> {
-    backgroundColor(btnCharacter1);
-    turnOffBackground(btnCharacter2,btnCharacter3,btnCharacter4);
-    int choice = 1;
-
-    });
-
-    btnCharacter2.setOnClickListener(view -> {
-      backgroundColor(btnCharacter2);
-      turnOffBackground(btnCharacter1,btnCharacter3,btnCharacter4);
-      int choice = 2;
-      data.putExtra(CHARACTER_PLAYER_ONE,choice);
-      setResult(RESULT_OK, data);
-      finish();
-
-    });
-
-    btnCharacter3.setOnClickListener(view -> {
-      backgroundColor(btnCharacter3);
-      turnOffBackground(btnCharacter1,btnCharacter2,btnCharacter4);
-      int choice = 3;
-    });
-
-    btnCharacter4.setOnClickListener(view -> {
-      backgroundColor(btnCharacter4);
-      turnOffBackground(btnCharacter1,btnCharacter2,btnCharacter3);
-      int choice = 4;
-    });
-
-
+    data.putExtra(INFO_CHARACTER_SELECTED, selectedCharacter.toString());
+    data.putExtra(INFO_PLAYER_SELECTED_CHARACTER, playerSelection);
+    setResult(u3a7left4deadSelectCharacter.RESULT_OK, data);
+    finish();
   }
 
   public void fullScreen() {
@@ -75,14 +101,6 @@ public class u3a7left4deadSelectCharacter extends AppCompatActivity {
 
   public void backgroundColor(ImageButton button) {
     button.setBackgroundColor(0x6F000000);
+
   }
-
-  public void turnOffBackground(ImageButton button, ImageButton button1, ImageButton button2) {
-    button.setBackgroundColor(0xFF5A1E1E);
-    button1.setBackgroundColor(0xFF5A1E1E);
-    button2.setBackgroundColor(0xFF5A1E1E);
-  }
-
-
-
 }
