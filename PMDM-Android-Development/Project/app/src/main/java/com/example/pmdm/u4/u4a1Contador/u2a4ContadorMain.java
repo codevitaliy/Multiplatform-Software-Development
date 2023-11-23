87856ba95d4678b44cbef6428e424d274537ccae
codevitaliy <codevitaliy@gmail.com>
2023-11-23 11:53:30 +0100

    wip u4a1

---
+++ PMDM-Android-Development/Project/app/src/main/java/com/example/pmdm/u4/u4a1Contador/u2a4ContadorMain.java
@@ -0,0 +1,16 @@
+package com.example.pmdm.u4.u4a1Contador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm.R;
+
+public class u2a4ContadorMain extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.u2a4_contador_main);
+  }
+}
